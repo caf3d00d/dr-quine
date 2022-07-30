243657db39e64ccbccb1c40e744dce01b3bcf7f5
Archer <leetpwnsroot@gmail.com>
2022-07-30 17:34:57 +0200

    Correcting comments

--- Go/Colleen/colleen.go
+++ Go/Colleen/colleen.go
@@ -4,13 +4,16 @@ import "fmt"
 
 func outsideFunc() string {
 	/*
-	* Outside comment
-	*/
-	a := "package main\n\nimport \"fmt\"\n\nfunc outsideFunc() string {\n\t/*\n\t* Outside comment\n\t*/\n\ta := %q\n\treturn a\n}\n\nfunc main() {\n\ta := outsideFunc()\n\tfmt.Printf(a, a)\n}\n"
+	 Outside comment
+	 */
+	a := "package main\n\nimport \"fmt\"\n\nfunc outsideFunc() string {\n\t/*\n\t Outside comment\n\t */\n\ta := %q\n\treturn a\n}\n\nfunc main() {\n\t/*\n\t Inside comment\n\t */\n\ta := outsideFunc()\n\tfmt.Printf(a, a)\n}\n"
 	return a
 }
 
 func main() {
+	/*
+	 Inside comment
+	 */
 	a := outsideFunc()
 	fmt.Printf(a, a)
 }
